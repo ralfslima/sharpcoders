68673ab82c93812caac142433d7c20a9017ce58e
ralflima <ralf.lima@gmail.com>
2023-12-20 19:52:40 -0300

    FastTrack

--- FastTrack/back-end/index.js
+++ FastTrack/back-end/index.js
@@ -1,16 +1,60 @@
 // Express -> Framework (npm i express)
 // Nodemon -> Executar projeto (npm i nodemon)
+// JSONWEBTOKEN -> JWT (npm i jsonwebtoken)
 
 // Importações
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 // Objeto app (responsável pelas rotas)
 const app = express();
 
+// Ter acesso a um objeto via requisição
+app.use(express.json());
+
 // Rotas
+app.post('/', function(req, res){
+
+    // Obter dados via body
+    let usuario = req.body.usuario;
+    let senha = req.body.senha;
+
+    // Condicional
+    if(usuario === 'Ralf' && senha === 123){
+        // Criar token
+        let gerarToken = jwt.sign({'usuario':usuario}, 'ima', {expiresIn:'1m'});
+
+        // Retorno
+        res.status(200).json({'token':gerarToken});
+    }else{
+        // Retorno
+        res.status(400).json({'msg':'Usuário ou senha incorretos'});
+    }
+
+});
+
 app.get('/', function(req, res){
-    res.write('Hello World!');
-    res.end();
+    // Obter token via Bearer
+    const bearer = req.headers['authorization'];
+
+    // Validar token
+    if(bearer == undefined){
+        res.status(401).json({'msg':'É necessário informar um token'});
+    }else{
+        // Separar a estrutura do token enviado
+        let tokenEnviado = bearer.split(' ');
+
+        // Validar token
+        jwt.verify(tokenEnviado[1], 'ima', (erro) => {
+            if(erro){
+                res.status(401).json({'msg':'Token inválido'});
+            }else{
+                res.status(200).json({'msg':'Token validado!'});
+            }
+        });
+
+    }
+
 });
 
 // Rota do servidor
