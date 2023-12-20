8cb2e8746a25cd4fe5a80007f9a5ec703d77ae59
ralflima <ralf.lima@gmail.com>
2023-12-20 19:59:59 -0300

    FastTrack

--- FastTrack/back-end/index.js
+++ FastTrack/back-end/index.js
@@ -1,13 +1,18 @@
 // Express -> Framework (npm i express)
 // Nodemon -> Executar projeto (npm i nodemon)
 // JSONWEBTOKEN -> JWT (npm i jsonwebtoken)
+// CORS -> Segurança dos navegadores, para não realizar requisições em portas diferentes (npm i cors)
 
 // Importações
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors')
 
 // Objeto app (responsável pelas rotas)
 const app = express();
+
+// Informar ao sistema, que o CORS deverá ser habilitado
+app.use(cors());
 
 // Ter acesso a um objeto via requisição
 app.use(express.json());
@@ -32,6 +37,10 @@ app.post('/', function(req, res){
     }
 
 });
+
+app.get('/teste', function(req, res){
+    res.write('teste');
+})
 
 app.get('/', function(req, res){
     // Obter token via Bearer
