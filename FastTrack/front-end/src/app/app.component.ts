9a76abdef780a61add41294fadec251856f7f3df
ralflima <ralf.lima@gmail.com>
2023-12-20 20:08:07 -0300

    FastTrack

--- FastTrack/front-end/src/app/app.component.ts
+++ FastTrack/front-end/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +13,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'front-end';
+
+  // Construtor
+  constructor(private http:HttpClient){}
+
+  // Inicialização
+  ngOnInit(){
+    // this.criarToken().subscribe(retorno => {
+    //   // LocalStorage
+    //   localStorage.setItem('token', retorno.token);
+    //   // Console
+    //   console.log(retorno);
+    // });
+  }
+
+  // Método para criar um token
+  criarToken():Observable<any>{
+    return this.http.post<any>('http://www.localhost:8080', 
+    {'usuario':'Ralf', 'senha':123});
+  }
 }
